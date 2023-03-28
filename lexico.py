4ec64a4897178f2cbc71310ae8cf9f682a029013
Sersasj <sasjsergioalvarezjunior@gmail.com>
2023-03-27 21:57:57 -0300

    primeiroo

---
+++ lexico.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+
+print("aaaaa")
+
+# Ideia inicial, fazer igualzinho C
+# Usando a ferramenta py.lex
+
+import ply.lex as lex
+
+tokens = (
+   'ID', # identificador
+   'NUMBER', # constantes numéricas
+   'OP', # operadores
+   
+)
+
+
