33e2ffcf574dbd0b7758ac10e5eae9bc89b818e8
Sersasj <sasjsergioalvarezjunior@gmail.com>
2023-03-31 09:19:21 -0300

    lexico terminado ??

--- lexico.py
+++ lexico.py
@@ -22,8 +22,26 @@ tokens = (
    'GREATER_THAN_EQUAL',
    'AND',
    'OR',
-   'NOT'
+   'NOT',
+   'RIGHT_PAREN',
+   'LEFT_PAREN',
+   'RIGHT_BRACE',
+   'LEFT_BRACE',
+   'RIGHT_BRACKET',
+   'LEFT_BRACKET',
+   'COMMA',
+   'SEMICOLON'
+   
 )
+
+reserved = {
+   'if' : 'IF',
+   'then' : 'THEN',
+   'else' : 'ELSE',
+   'while' : 'WHILE',
+   'print' : 'PRINT'
+}
+
 
 t_ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
 t_NUMBER= r'\d+' 
@@ -39,25 +57,135 @@ t_GREATER_THAN_EQUAL = r'>='
 t_AND = r'&&'
 t_OR = r'\|\|'
 t_NOT = r'!'
-
+t_RIGHT_PAREN = r'\)'
+t_LEFT_PAREN = r'\('
+t_RIGHT_BRACE = r'\}'
+t_LEFT_BRACE = r'\{'
+t_RIGHT_BRACKET = r'\]'
+t_LEFT_BRACKET = r'\['
+t_COMMA = r','
+t_SEMICOLON = r';'
 # Ignora espaço e quebra de linha
 t_ignore = ' \t\n'
+
 def t_error(t): 
     print("Cabou-se, deu erro em", t)
     t.lexer.skip(1) 
 
 
-
-
-
 def testes():
     lexer = lex.lex()     
     # Declarar variavel 
-    data = "aaaaa = 1" 
+    data = "aaaaa" 
     lexer.input(data) 
     tokens = [tok.type for tok in lexer]
-    assert tokens == ['ID', 'EQUALS', 'NUMBER']
+    assert tokens == ['ID']
+    
+    data = "1"
+    lexer.input(data)
+    tokens = [tok.type for tok in lexer]
+    assert tokens == ['NUMBER']
+   
+    
+    data = "+"
+    lexer.input(data)
+    tokens = [tok.type for tok in lexer]
+    assert tokens == ['PLUS']
 
+    data = "*"
+    lexer.input(data)
+    tokens = [tok.type for tok in lexer]
+    assert tokens == ['TIMES']    
+    
+    data = "-"
+    lexer.input(data)
+    tokens = [tok.type for tok in lexer]
+    assert tokens == ['MINUS']        
+    
+    data = "/"
+    lexer.input(data)
+    tokens = [tok.type for tok in lexer]
+    assert tokens == ['DIVIDE']        
+
+    data = "="
+    lexer.input(data)
+    tokens = [tok.type for tok in lexer]
+    assert tokens == ['EQUALS']
+           
+    data = "<"
+    lexer.input(data)
+    tokens = [tok.type for tok in lexer]
+    assert tokens == ['LESS_THAN']
+
+    data = ">"
+    lexer.input(data)
+    tokens = [tok.type for tok in lexer]
+    assert tokens == ['GREATER_THAN']
+
+    data = "<="
+    lexer.input(data)
+    tokens = [tok.type for tok in lexer]
+    assert tokens == ['LESS_THAN_EQUAL']
+
+    data = ">="
+    lexer.input(data)
+    tokens = [tok.type for tok in lexer]
+    assert tokens == ['GREATER_THAN_EQUAL']
+
+    data = "&&"
+    lexer.input(data)
+    tokens = [tok.type for tok in lexer]
+    assert tokens == ['AND']
+
+    data = "||"
+    lexer.input(data)
+    tokens = [tok.type for tok in lexer]
+    assert tokens == ['OR']
+
+    data = "!"
+    lexer.input(data)
+    tokens = [tok.type for tok in lexer]
+    assert tokens == ['NOT']
+
+    data = "("
+    lexer.input(data)
+    tokens = [tok.type for tok in lexer]
+    assert tokens == ['LEFT_PAREN']
+
+    data = ")"
+    lexer.input(data)
+    tokens = [tok.type for tok in lexer]
+    assert tokens == ['RIGHT_PAREN']
+
+    data = "{"
+    lexer.input(data)
+    tokens = [tok.type for tok in lexer]
+    assert tokens == ['LEFT_BRACE']
+
+    data = "}"
+    lexer.input(data)
+    tokens = [tok.type for tok in lexer]
+    assert tokens == ['RIGHT_BRACE']
+
+    data = "["
+    lexer.input(data)
+    tokens = [tok.type for tok in lexer]
+    assert tokens == ['LEFT_BRACKET']
+
+    data = "]"
+    lexer.input(data)
+    tokens = [tok.type for tok in lexer]
+    assert tokens == ['RIGHT_BRACKET']
+
+    data = ","
+    lexer.input(data)
+    tokens = [tok.type for tok in lexer]
+    assert tokens == ['COMMA']
+
+    data = ";"
+    lexer.input(data)
+    tokens = [tok.type for tok in lexer]
+    assert tokens == ['SEMICOLON']
 
 
 testes()
