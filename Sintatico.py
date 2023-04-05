33e99192004e4586b19003c698b56bdbae415a8d
Felipe Roveroni de Lima <54330906+FelipeRoveroniLima@users.noreply.github.com>
2023-04-05 19:51:37 -0300

    literalmente_eu_
    Avanço no Sintático, tem que ver como tratar o int e float separados

--- Sintatico.py
+++ Sintatico.py
@@ -1,12 +1,7 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Apr  2 14:46:48 2023
 
-@author: sergi
-"""
 import ply.lex as lex
 import ply.yacc as yacc
-
 from lexico import Lexico
 
 class Parser:
@@ -17,38 +12,69 @@ class Parser:
         
     def p_statement_assign(self, p):
         'statement : ID EQUALS expr SEMICOLON'
+        p[0] = p[3]
     
     def p_expr_binop(self, p):
         '''expr : expr PLUS expr
                 | expr MINUS expr
                 | expr TIMES expr
                 | expr DIVIDE expr'''
+        if (p[2] == '+'):
+            p[0] = p[1] + p[3]
+        elif (p[2] == '-'):
+            p[0] = p[1] - p[3]
+        elif (p[2] == '*'):
+            p[0] = p[1] * p[3]
+        else:
+            p[0] = p[1] / p[3]
     
     def p_expr_relational(self, p):
         '''expr : expr LESS_THAN expr
                 | expr LESS_THAN_EQUAL expr
                 | expr GREATER_THAN expr
                 | expr GREATER_THAN_EQUAL expr'''
+        p[0] = ('COMPARE', p[2], p[1], p[3])
     
     def p_expr_logical(self, p):
         '''expr : expr AND expr
                 | expr OR expr
                 | NOT expr'''
+        if len(p) == 4:
+             # Operadores AND e OR
+            p[0] = ('LOGICAL_OP', p[2], p[1], p[3])
+        else:
+            # Operador NOT
+            p[0] = ('LOGICAL_OP', 'NOT', p[2])
     
     def p_expr_group(self, p):
         'expr : LEFT_PAREN expr RIGHT_PAREN'
+        p[0] = p[2]
     
     def p_expr_id(self, p):
         'expr : ID'
+        p[0] = p[1]
     
     def p_expr_num(self, p):
         'expr : NUMBER'
+        p[0] = int(p[1])
     
     def p_error(self, p):
         print("Syntax error in input!")
         
     def testes(self, texto):
         return self.parser.parse(texto, lexer=self.lexico.lexico)
+    
 p = Parser()
-result = p.parser.parse("x = 3 + 4;", lexer=p.lexico.lexico)
+result = p.parser.parse("x = 3 + 4;", lexer=p.lexico.lexico) # 7
+print()
 print(result)
+print()
+result = p.parser.parse("x = (3 * 2) + 4;", lexer=p.lexico.lexico) # 10
+print(result)
+print()
+result = p.parser.parse("x = 1 && 2;", lexer=p.lexico.lexico) # ('LOGICAL_OP', '&&', 1, 2)
+print(result)
+print()
+result = p.parser.parse("x = 3 >= 4;", lexer=p.lexico.lexico) # ('COMPARE', '>=', 3, 4)
+print(result)
+print()
